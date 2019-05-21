bc9937478b43a2f994a9a24348359e19e35c7613
saveroz <muhammadsavero@gmail.com>
2019-05-21 22:13:55 +0700

    ngumpulin tugas

---
+++ exercise-3.js
@@ -0,0 +1,11 @@
+var name='Nina';
+var peran='ksatria';
+
+
+if (peran==='ksatria' || peran==='tabib' ||peran==='penyihir'){
+    console.log(`Selamat datang di Dunia Proxytia, ${name}`);
+    console.log(`Halo ${peran} ${name}, kamu dapat menyerang dengan senjatamu!`);
+}  
+else{
+    console.log(`Halo ${name}, Pilih peranmu untuk memulai game!`);
+}
---
+++ exercise-4.js
@@ -0,0 +1,48 @@
+var tanggal;
+var bulan;
+var tahun;
+
+tanggal=1;
+bulan=9;
+tahun=1877;
+
+switch(bulan){
+    case 1:
+    bulan='januari';
+    break;
+    case 2:
+    bulan='februari'
+    break;
+    case 3:
+    bulan='maret'
+    break;
+    case 4:
+    bulan='april'
+    break;
+    case 5:
+    bulan='mei'
+    break;
+    case 6:
+    bulan='juni'
+    break;
+    case 7:
+    bulan='juli'
+    break;
+    case 8:
+    bulan='agustus'
+    break;
+    case 9:
+    bulan='september'
+    break;
+    case 10:
+    bulan='oktober'
+    break;
+    case 11:
+    bulan='november'
+    break;
+    case 12:
+    bulan='desember'
+    break;
+}
+
+console.log(`${tanggal} ${bulan} ${tahun}`);
