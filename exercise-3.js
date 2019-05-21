605bb06f3499bef2dfdd9140a06515b5786862fd
saveroz <muhammadsavero@gmail.com>
2019-05-21 22:52:08 +0700

    final

--- exercise-3.js
+++ exercise-3.js
@@ -1,5 +1,5 @@
 var name='savero';
-var peran='';
+var peran='ksatria';
 
 if (!name && !peran){
 console.log("Nama harus diisi!");
@@ -7,16 +7,16 @@ console.log("Nama harus diisi!");
 else if(!peran){
 console.log(`Halo ${name}, Pilih peranmu untuk memulai game!`);
 }    
-else if (peran==='ksatria'){
+else if (name && peran==='ksatria'){
     console.log(`Selamat datang di Dunia Proxytia, ${name}`);
     console.log(`Halo ${peran} ${name}, kamu dapat menyerang dengan senjatamu!`);
 }  
-else if(peran==='tabib'){
+else if(name && peran==='tabib'){
     console.log(`Selamat datang di Dunia Proxytia, ${name}`);
     console.log(`Halo ${peran} ${name}, kamu akan membantu temanmu yang terluka.`);
 
 }
-else if(peran==='penyihir'){
+else if(name && peran==='penyihir'){
     console.log(`Selamat datang di Dunia Proxytia, ${name}`);
     console.log(`Halo ${peran} ${name}, ciptakan keajaiban yang membantu kemenanganmu!`);
 }
